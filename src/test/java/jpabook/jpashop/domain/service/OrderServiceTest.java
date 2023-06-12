27521a61bb47ba908eaea6f81acc8933756918d6
HoyaXD <ssinna50@gmail.com>
2023-06-13 01:14:56 +0900

    commit and push 5test

--- src/test/java/jpabook/jpashop/domain/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/domain/service/OrderServiceTest.java
@@ -52,7 +52,7 @@ public class OrderServiceTest {
         Assert.assertEquals("주문한 상품 종류 수가 정확해야 한다.", 1, getOrder.getOrderItems().size());
         Assert.assertEquals("주문 가격은 가격 * 수량이다.", 10000 * orderCount, getOrder.getTotalPrice());
         Assert.assertEquals("주문 수량만큼 재고가 줄어야 한다.", 8, book.getStockQuantity());
-
+        System.out.println("test2");
 
     }
 /*
